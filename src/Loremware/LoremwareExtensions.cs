e3a8cfbece7c9dbbdf1d59165443024743f85df1
thekfactr <thekfactor83@gmail.com>
2017-08-13 22:14:51 -0400

    fluent extn. and other classes

---
+++ src/Loremware/LoremwareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Loremware
+{
+    public static class LoremwareExtensions
+    {
+        public static IApplicationBuilder UseLoremware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoremwareMiddleware>();
+        }
+    }
+}
