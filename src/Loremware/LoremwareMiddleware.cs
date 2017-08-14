97b1077e6ba727e120744b72b4af04e17dfb85a0
thekfactr <thekfactor83@gmail.com>
2017-08-13 22:07:40 -0400

    initial commit

---
+++ src/Loremware/LoremwareMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Loremware
+{
+    public class LoremwareMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoremwareMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
