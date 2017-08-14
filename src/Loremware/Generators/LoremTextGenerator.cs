f8e5fb79719a1b5ae43fbfeccb5b528f0510d606
thekfactr <thekfactor83@gmail.com>
2017-08-13 23:55:26 -0400

    added a randomized text generator

--- src/Loremware/Generators/LoremTextGenerator.cs
+++ src/Loremware/Generators/LoremTextGenerator.cs
@@ -1,11 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace Loremware.Generators
 {
-    public class LoremTextGenerator
+    public static class LoremTextGenerator
     {
+        public static string GenerateLoremIpsumText(int minWords = 15, 
+                                                    int maxWords = 30, 
+                                                    int minSentences = 5, 
+                                                    int maxSentences = 10, 
+                                                    int numParagraphs = 3)
+        {
+            var words = new[]{"lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+                              "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
+                              "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"};
+
+            var rand = new Random();
+            int numSentences = rand.Next(maxSentences - minSentences)
+                + minSentences + 1;
+            int numWords = rand.Next(maxWords - minWords) + minWords + 1;
+
+            var result = new StringBuilder();
+
+            for (int p = 0; p < numParagraphs; p++)
+            {
+                result.Append("<p>");
+                for (int s = 0; s < numSentences; s++)
+                {
+                    for (int w = 0; w < numWords; w++)
+                    {
+                        if (w > 0) { result.Append(" "); }
+                        result.Append(words[rand.Next(words.Length)]);
+                    }
+                    result.Append(". ");
+                }
+                result.Append("</p>");
+            }
+
+            return result.ToString();
+        }
     }
 }
--- src/Loremware/Views/LoremPage.cs
+++ src/Loremware/Views/LoremPage.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Loremware.Generators;
+using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 
 namespace Loremware.Views
@@ -29,13 +30,13 @@ namespace Loremware.Views
             await context.Response.WriteAsync(@"</style>");
             await context.Response.WriteAsync(@"</head>");
             await context.Response.WriteAsync(@"<body>");
-            await context.Response.WriteAsync(@"<br/>
+            var paragraph1 = LoremTextGenerator.GenerateLoremIpsumText();
+            await context.Response.WriteAsync($@"<br/>
                                                 <div class=""container"">
                                                     <div class=""l-main"">
                                                         <div class=""l-main-body"">
                                                         <h3>Lorem Ipsum</h3>
-                                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                                        {paragraph1}
                                                         </div>
                                                     </div>
                                                 </div>");
